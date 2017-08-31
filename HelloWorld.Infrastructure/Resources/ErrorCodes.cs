65b458d0b1831321a441262bcbbeb5784ea8681c
Rustem Sharipov <rustemsharipov@outlook.com>
2017-08-30 22:17:46 -0500

    Add project files.

---
+++ HelloWorld.Infrastructure/Resources/ErrorCodes.cs
@@ -0,0 +1,24 @@
+﻿
+namespace HelloWorldInfrastructure.Resources
+{
+    /// <summary>
+    /// Contains the error codes for the application
+    /// </summary>
+    public class ErrorCodes
+    {
+        /// <summary>
+        ///     The General Error error code
+        /// </summary>
+        public const string GeneralError = "general-error";
+
+        /// <summary>
+        ///     The Today Data File Settings error code
+        /// </summary>
+        public const string TodaysDataFileSettingsKeyError = "todays-data-file-settings-error";
+
+        /// <summary>
+        ///     The Today Data File error code
+        /// </summary>
+        public const string TodayDataFileError = "today-data-file-error";
+    }
+}
